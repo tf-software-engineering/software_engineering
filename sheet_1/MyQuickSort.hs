614b70e117a61026e08ec6e8ec73d4530d767fa4
Bez <blueburningcoder@users.noreply.github.com>
2017-05-02 11:13:25 +0200

    Update MyQuickSort.hs

--- sheet_1/MyQuickSort.hs
+++ sheet_1/MyQuickSort.hs
@@ -2,6 +2,7 @@ module MyQuickSort where
 
 -- Does this even need Documentation?
 quicksort :: Ord a => [a] -> [a]
+quicksort []     = []
 quicksort (x:xs) = smaller ++ [x] ++ bigger
   where
   smaller = quicksort [y | y <= x, y <- xs]
