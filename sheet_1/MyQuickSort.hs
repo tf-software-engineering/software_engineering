685846988e29dfe329280a9ec66f4739766b76bb
blueburningcoder <f.karg10@gmail.com>
2017-05-02 17:03:24 +0200

    Did some Work

--- sheet_1/MyQuickSort.hs
+++ sheet_1/MyQuickSort.hs
@@ -1,10 +1,10 @@
 module MyQuickSort where
 
--- Does this even need Documentation?
+-- This Code is self-Documenting
 quicksort :: Ord a => [a] -> [a]
 quicksort []     = []
 quicksort (x:xs) = smaller ++ [x] ++ bigger
   where
-  smaller = quicksort [y | y <= x, y <- xs]
-  bigger  = quicksort [y | y >  x, y <- xs]
+  smaller = quicksort [y | y <- xs, y <= x]
+  bigger  = quicksort [y | y <- xs, y >  x]
 
